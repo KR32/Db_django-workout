e743a58dff500d6bfd4032f3d18d75c81d4ee18e
nova sangeeth <novasangeeth@outlook.com>
2021-01-20 01:51:28 +0530

    added confs for language and currenct tables for the admin panel viewing.

--- app_secondary/main/admin.py
+++ app_secondary/main/admin.py
@@ -11,9 +11,46 @@ class MainAdmin(admin.ModelAdmin):
             'widget':TinyMCE()
         },
     }
+
 class testoutAdmin(admin.ModelAdmin):
     list_display = ('name','age', 'phone_number', 'id')
 
+
+class LanguageAdmin(admin.ModelAdmin):
+    list_display =(
+        'language',
+        'comments'
+    )
+
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = (
+        'currency_name',
+        'country_name',
+        'currency_code',
+        'currency_symbol',
+        'comments'
+    )
+    exclude = ( 
+        'currency_id',
+    )
+    formfield_overrides = {
+        models.TextField: {
+            'widget':TinyMCE()
+        },
+    }
+
+class CountryInformationAdmin(admin.ModelAdmin):
+    list_display =(
+        'country_name',
+        'nationality',
+        'dailing_code',
+        # 'currency_id',
+        # 'language_id'
+    )
+
+    
+
 admin.site.register(testout, MainAdmin)
-admin.site.register(Language)
-admin.site.register(country_information)
+admin.site.register(Language, LanguageAdmin)
+admin.site.register(Currency, CurrencyAdmin)
+admin.site.register(country_information, CountryInformationAdmin)
