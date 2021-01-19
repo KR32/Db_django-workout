754891356f7b01c95e2cb35dfd0bc8a23168281b
nova sangeeth <novasangeeth@outlook.com>
2021-01-20 01:51:01 +0530

    changes to the models .

--- app_1/main/models.py
+++ app_1/main/models.py
@@ -16,10 +16,21 @@ class testout(models.Model):
 class Language(models.Model):
     language_id = models.IntegerField(primary_key=True)
     language = models.CharField(max_length=128, null=True)
-    comments = models.CharField(max_length=128, null = True)
+    comments = models.TextField(max_length=128, null = True)
 
     def __repr__(self) -> str:
         return self.language
+
+class Currency(models.Model):
+    currency_id = models.IntegerField(primary_key=True)
+    country_name = models.CharField(max_length=100, null=True)
+    currency_code = models.CharField(max_length=30, null=True)
+    currency_name = models.CharField(max_length=100, null=True)
+    currency_symbol = models.CharField(max_length=30, null=True)
+    comments = models.TextField(max_length=255, null=True)
+    
+    def __str__(self) -> str:
+        return self.currency_name
 
 class country_information(models.Model):
     country_id =  models.IntegerField(primary_key=True)
@@ -27,7 +38,9 @@ class country_information(models.Model):
     nationality = models.CharField(max_length=128, null=True)
     dailing_code = models.CharField(max_length=128, null=True)
     country_name = models.CharField(max_length=128, null=True)
-    language_id = models.ForeignKey('Language', on_delete=models.CASCADE)
+    currency_id = models.ForeignKey('Currency', on_delete=models.CASCADE, null=True)
+    language_id = models.ForeignKey('Language', on_delete=models.CASCADE, null=True)
 
     def __repr__(self) -> str:
         return self.country_name
+
