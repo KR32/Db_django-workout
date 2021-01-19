2a8d400fea3957256e37c2fb45c027c1e868af55
nova sangeeth <novasangeeth@outlook.com>
2021-01-20 00:38:40 +0530

    Configs for the richtext editor in the django admin

--- app_secondary/main/admin.py
+++ app_secondary/main/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-
+from tinymce.widgets import TinyMCE
 # Register your models here.
+from .models import *
 
-from .models import testout
 
+
+class MainAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {
+            'widget':TinyMCE()
+        },
+    }
 class testoutAdmin(admin.ModelAdmin):
     list_display = ('name','age', 'phone_number', 'id')
 
-admin.site.register(testout, testoutAdmin)
+admin.site.register(testout, MainAdmin)
+admin.site.register(Language)
+admin.site.register(country_information)
