f20ceca8e61b0f68e1a1b876b9a4d74f4d458931
nova sangeeth <novasangeeth@outlook.com>
2021-01-20 00:37:46 +0530

    added new models with ForeignKeys and TextAreas to test the rich text editor.

--- app_1/main/models.py
+++ app_1/main/models.py
@@ -6,5 +6,28 @@ from django.db import models
 class testout(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
     name = models.CharField(max_length=100, null=True)
-    age = models.PositiveIntegerField( null=True)
-    phone_number = models.CharField(max_length=12,  null=True)
+    age = models.PositiveIntegerField(null = True)
+    phone_number = models.CharField(max_length=12, null=True)
+    notes = models.TextField(null=True)
+        
+    def __str__(self) -> str:
+        return self.name
+
+class Language(models.Model):
+    language_id = models.IntegerField(primary_key=True)
+    language = models.CharField(max_length=128, null=True)
+    comments = models.CharField(max_length=128, null = True)
+
+    def __repr__(self) -> str:
+        return self.language
+
+class country_information(models.Model):
+    country_id =  models.IntegerField(primary_key=True)
+    country_name = models.CharField(max_length=128, null=True)
+    nationality = models.CharField(max_length=128, null=True)
+    dailing_code = models.CharField(max_length=128, null=True)
+    country_name = models.CharField(max_length=128, null=True)
+    language_id = models.ForeignKey('Language', on_delete=models.CASCADE)
+
+    def __repr__(self) -> str:
+        return self.country_name
