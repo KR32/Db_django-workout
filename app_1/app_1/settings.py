f333bce608ed3a958b632ff3ea8e889c9dd7f36a
nova sangeeth <novasangeeth@outlook.com>
2021-01-21 00:23:13 +0530

    new configs for adding support for postgresql DB.

--- app_1/app_1/settings.py
+++ app_1/app_1/settings.py
@@ -74,11 +74,23 @@ WSGI_APPLICATION = 'app_1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'code_dev_admin',
+        'USER': 'postgres',
+        'PASSWORD': 'nova',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+
 }
 
 
--- app_secondary/app_secondary/settings.py
+++ app_secondary/app_secondary/settings.py
@@ -91,14 +91,25 @@ WSGI_APPLICATION = 'app_secondary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+  #      'NAME': '/home/novasangeeth/Code--dev/django-admin-workout/app_1/db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+ #   }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/home/novasangeeth/Code--dev/django-admin-workout/app_1/db.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'code_dev_admin',
+        'USER': 'postgres',
+        'PASSWORD': 'nova',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
-}
 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
