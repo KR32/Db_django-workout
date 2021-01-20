5eb20154075135795ddd69b8279131214347d138
nova sangeeth <novasangeeth@outlook.com>
2021-01-21 00:22:01 +0530

    added new models section_templates.

--- app_1/main/models.py
+++ app_1/main/models.py
@@ -44,3 +44,29 @@ class country_information(models.Model):
     def __repr__(self) -> str:
         return self.country_name
 
+
+class section_template(models.Model):
+    section_id = models.AutoField(primary_key=True)
+    section_name = models.CharField(max_length=128, null=True)
+    section_desc = models.TextField(null=True)
+    display_order = models.IntegerField(null=True)
+    display_name = models.CharField(max_length=128)
+
+    def __str__(self) -> str:
+        return self.section_name
+
+
+
+class JobDescriptionTemplate(models.Model):
+    job_desc_id = models.AutoField (primary_key=True)
+    description_title = models.CharField(max_length=255, null=False)
+    # organisation_description = models.Text, nullable=False)
+    organisation_job_description = models.TextField(null=True)
+    # qualifications_required = models.Text, nullable=False)
+    # skills_required = models.Text, nullable=False)
+    # essential_information = models.Text, nullable=False)
+    # desired_information = models.Text, nullable=False)
+    comments = models.TextField(null=True) 
+
+    def __str__(self) -> str:
+        return self.description_title
