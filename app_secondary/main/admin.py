783b0a19182982c5d258a0f6e0714a324f4af8ce
nova sangeeth <novasangeeth@outlook.com>
2021-01-22 01:10:13 +0530

    formated the code .

--- app_secondary/main/admin.py
+++ app_secondary/main/admin.py
@@ -1,117 +1,129 @@
 from django.contrib import admin
 from tinymce.widgets import TinyMCE
+
 # Register your models here.
 from .models import *
 
-admin.site.site_header = 'OHR Administration.'
+admin.site.site_header = "OHR Administration."
+
 
 @admin.register(testout)
 class MainAdmin(admin.ModelAdmin):
     formfield_overrides = {
-        models.TextField: {
-            'widget':TinyMCE()
-        },
+        models.TextField: {"widget": TinyMCE()},
     }
+
 
 class testoutAdmin(admin.ModelAdmin):
     # list_display = ('name','age', 'phone_number')
     pass
 
+
 @admin.register(Language)
 class LanguageAdmin(admin.ModelAdmin):
-    list_display =(
-        'language',
-        'comments'
-    )
+    list_display = ("language", "comments")
+
+
 @admin.register(Currency)
 class CurrencyAdmin(admin.ModelAdmin):
     list_display = (
-        'currency_name',
-        'country_name',
-        'currency_code',
-        'currency_symbol',
-        'comments'
+        "currency_name",
+        "country_name",
+        "currency_code",
+        "currency_symbol",
+        "comments",
     )
-    exclude = ( 
-        'currency_id',
-    )
+    exclude = ("currency_id",)
     search_fields = (
-        'country_name',
-        'currency_name',
-        'currency_code',
+        "country_name",
+        "currency_name",
+        "currency_code",
     )
     formfield_overrides = {
-        models.TextField: {
-            'widget':TinyMCE()
-        },
+        models.TextField: {"widget": TinyMCE()},
     }
+
+
 @admin.register(country_information)
 class CountryInformationAdmin(admin.ModelAdmin):
-    list_display =(
-        'country_name',
-        'nationality',
-        'dailing_code',
+    list_display = (
+        "country_name",
+        "nationality",
+        "dailing_code",
         # 'currency_id',
         # 'language_id'
     )
+
+
 @admin.register(section_template)
 class SectionTemplateAdmin(admin.ModelAdmin):
-    list_display = (
-        'section_name',
-    )
+    list_display = ("section_name",)
     formfield_overrides = {
-        models.TextField: {
-            'widget':TinyMCE()
-        },
+        models.TextField: {"widget": TinyMCE()},
     }
     # exclude = ('section_id',)
-@admin.register(JobDescriptionTemplate)    
+
+
+@admin.register(JobDescriptionTemplate)
 class JobDescTemplateAdmin(admin.ModelAdmin):
-    list_display = (    
-        'description_title',
-    )
+    list_display = ("description_title",)
     formfield_overrides = {
-        models.TextField: {
-            'widget': TinyMCE()
-        },
+        models.TextField: {"widget": TinyMCE()},
     }
     # exclude = ('job_desc_id',)
+
 
 @admin.register(PqrDetail)
 class PQRDetailTemplateAdmin(admin.ModelAdmin):
     list_display = (
-        'description',
-        'surgical',
-        'min_years_exp',
-        'min_years_exp_for_nationals',
-        'specialist_registration_held',
-        'specialist_reg_required_for_nationals',
-        'specialist_registration_years',
-        'check_equivalence',
-        'comments'
-        )
+        "description",
+        "surgical",
+        "min_years_exp",
+        "min_years_exp_for_nationals",
+        "specialist_registration_held",
+        "specialist_reg_required_for_nationals",
+        "specialist_registration_years",
+        "check_equivalence",
+        "comments",
+    )
+
 
 @admin.register(PqrHeader)
 class PQRHeaderAdmin(admin.ModelAdmin):
     list_display = (
-        'country_id', 'issue_authority_id', 'pqr_name', 'issue_date', 'version_no', 'description', 'comments'
+        "country_id",
+        "issue_authority_id",
+        "pqr_name",
+        "issue_date",
+        "version_no",
+        "description",
+        "comments",
     )
+
 
 @admin.register(IssueAuthority)
 class IssueAuthorityAdmin(admin.ModelAdmin):
     list_display = (
-        'authority_name',
-        'acronym',
-        'address',
-        'website',
-        'description',
-        'licensing', 
-        'qualification'
+        "authority_name",
+        "acronym",
+        "address",
+        "website",
+        "description",
+        "licensing",
+        "qualification",
     )
+
 
 @admin.register(Qualification)
 class QualificationAdmin(admin.ModelAdmin):
     list_display = (
-        'qualification', 'qualification_level', 'description', 'acronym', 'equivalent_qualification',
-        'specialist_registration_reqd', 'by_exam', 'course_duration_reqd', 'display_order'
+        "qualification",
+        "qualification_level",
+        "description",
+        "acronym",
+        "equivalent_qualification",
+        "specialist_registration_reqd",
+        "by_exam",
+        "course_duration_reqd",
+        "display_order",
     )
