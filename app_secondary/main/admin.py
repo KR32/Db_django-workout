92ddebbf8a4f1141527ef1c1b780263717508583
nova sangeeth <novasangeeth@outlook.com>
2021-01-22 00:54:28 +0530

    changes to the admin panel.

--- app_secondary/main/admin.py
+++ app_secondary/main/admin.py
@@ -5,7 +5,6 @@ from .models import *
 
 admin.site.site_header = 'OHR Administration.'
 
-
 class MainAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.TextField: {
@@ -14,7 +13,8 @@ class MainAdmin(admin.ModelAdmin):
     }
 
 class testoutAdmin(admin.ModelAdmin):
-    list_display = ('name','age', 'phone_number', 'id')
+    # list_display = ('name','age', 'phone_number')
+    pass
 
 
 class LanguageAdmin(admin.ModelAdmin):
@@ -71,6 +71,44 @@ class JobDescTemplateAdmin(admin.ModelAdmin):
     }
     # exclude = ('job_desc_id',)
 
+@admin.register(PqrDetail)
+class PQRDetailTemplateAdmin(admin.ModelAdmin):
+    list_display = (
+        'description',
+        'surgical',
+        'min_years_exp',
+        'min_years_exp_for_nationals',
+        'specialist_registration_held',
+        'specialist_reg_required_for_nationals',
+        'specialist_registration_years',
+        'check_equivalence',
+        'comments'
+        )
+
+@admin.register(PqrHeader)
+class PQRHeaderAdmin(admin.ModelAdmin):
+    list_display = (
+        'country_id', 'issue_authority_id', 'pqr_name', 'issue_date', 'version_no', 'description', 'comments'
+    )
+
+@admin.register(IssueAuthority)
+class IssueAuthorityAdmin(admin.ModelAdmin):
+    list_display = (
+        'authority_name',
+        'acronym',
+        'address',
+        'website',
+        'description',
+        'licensing', 
+        'qualification'
+    )
+
+@admin.register(Qualification)
+class QualificationAdmin(admin.ModelAdmin):
+    list_display = (
+        'qualification', 'qualification_level', 'description', 'acronym', 'equivalent_qualification',
+        'specialist_registration_reqd', 'by_exam', 'course_duration_reqd', 'display_order'
+    )
 
 admin.site.register(testout, MainAdmin)
 admin.site.register(Language, LanguageAdmin)
