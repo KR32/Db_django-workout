f994e5c4ca8c3ef1c5b14ac68cd85d0b33d5ca23
nova sangeeth <novasangeeth@outlook.com>
2021-01-22 01:09:44 +0530

    added all the admin.register lines to the respective decorators.

--- app_secondary/main/admin.py
+++ app_secondary/main/admin.py
@@ -5,6 +5,7 @@ from .models import *
 
 admin.site.site_header = 'OHR Administration.'
 
+@admin.register(testout)
 class MainAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.TextField: {
@@ -16,13 +17,13 @@ class testoutAdmin(admin.ModelAdmin):
     # list_display = ('name','age', 'phone_number')
     pass
 
-
+@admin.register(Language)
 class LanguageAdmin(admin.ModelAdmin):
     list_display =(
         'language',
         'comments'
     )
-
+@admin.register(Currency)
 class CurrencyAdmin(admin.ModelAdmin):
     list_display = (
         'currency_name',
@@ -34,12 +35,17 @@ class CurrencyAdmin(admin.ModelAdmin):
     exclude = ( 
         'currency_id',
     )
+    search_fields = (
+        'country_name',
+        'currency_name',
+        'currency_code',
+    )
     formfield_overrides = {
         models.TextField: {
             'widget':TinyMCE()
         },
     }
-
+@admin.register(country_information)
 class CountryInformationAdmin(admin.ModelAdmin):
     list_display =(
         'country_name',
@@ -48,7 +54,7 @@ class CountryInformationAdmin(admin.ModelAdmin):
         # 'currency_id',
         # 'language_id'
     )
-
+@admin.register(section_template)
 class SectionTemplateAdmin(admin.ModelAdmin):
     list_display = (
         'section_name',
@@ -59,7 +65,7 @@ class SectionTemplateAdmin(admin.ModelAdmin):
         },
     }
     # exclude = ('section_id',)
-    
+@admin.register(JobDescriptionTemplate)    
 class JobDescTemplateAdmin(admin.ModelAdmin):
     list_display = (    
         'description_title',
@@ -109,10 +115,3 @@ class QualificationAdmin(admin.ModelAdmin):
         'qualification', 'qualification_level', 'description', 'acronym', 'equivalent_qualification',
         'specialist_registration_reqd', 'by_exam', 'course_duration_reqd', 'display_order'
     )
-
-admin.site.register(testout, MainAdmin)
-admin.site.register(Language, LanguageAdmin)
-admin.site.register(Currency, CurrencyAdmin)
-admin.site.register(country_information, CountryInformationAdmin)
-admin.site.register(section_template, SectionTemplateAdmin)
-admin.site.register(JobDescriptionTemplate, JobDescTemplateAdmin)
