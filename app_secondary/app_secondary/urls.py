6bfeeaeaf25a720a5854795c2d1a92c9395d8a93
nova sangeeth <novasangeeth@outlook.com>
2021-01-23 00:42:48 +0530

    changes to the urls for chained fields.

--- app_secondary/app_secondary/urls.py
+++ app_secondary/app_secondary/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('chaining/', include('smart_selects.urls')),
     path('tinymce/', include('tinymce.urls')),
     path('admin/', admin.site.urls),
 ]
